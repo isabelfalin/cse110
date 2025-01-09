30c3deb1525a602123abc9642c5c1d76e0ffb1e1
isabelfalin <fal24007@byui.edu>
2025-01-09 13:17:53 -0700

    assignment 1

---
+++ favoritecolor.py
@@ -0,0 +1,20 @@
+color = input("Please type your favorite color: ")
+
+print()
+print(f"Your favorite color is: {color}!!")
+
+print()
+animal = input("Please state you're favorite animal: ")
+
+print()
+print(f"Your favorite animal is a(n): {animal} :D" )
+
+answer = input(f"Do you like {color} {animal}s? ")
+
+print()
+if answer == "yes":
+    print("Oh good! I like them too!")
+elif answer == "maybe":
+    print("Give me a better answer!")
+else: 
+    print("Repent, and try again.")
