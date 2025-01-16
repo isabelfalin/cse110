e22a5066259e20a9c42299c5ffe07187d2123d53
isabelfalin <fal24007@byui.edu>
2025-01-16 12:11:42 -0700

    whoooo

--- projects/madlibs.py
+++ projects/madlibs.py
@@ -16,12 +16,4 @@ print()
 print("Here Is Your Story:")
 print()
 
-print(f"""The other day, I was really in trouble. It all started when I saw a very
-{adjective} {animal} {verb_one} down the hallway. "{exclamation.capitalize()}!" I yelled. But all I 
-could think to do was to {verb_two} over and over. Miraculously, 
-that caused it to stop, but not before it tried to {verb_three} 
-right in front of my family. Filled with {emotion} I ran over to the wall and picked up the {object} sitting there,
-I {action} towards the {animal} and threw my new weapon, yelling "{exclamation.capitalize()}!"as it flew through the air.""")
-
-
-
+print(f"The other day, I was really in trouble. It all started when I saw a very {adjective} {animal} {verb_one} down the hallway. \"{exclamation.capitalize()}!\" I yelled. But all I could think to do was to {verb_two} over and over. Miraculously, that caused it to stop, but not before it tried to {verb_three} right in front of my family. Filled with {emotion} I ran over to the wall and picked up the {object} sitting there, I {action} towards the {animal} and threw my new weapon, yelling \"{exclamation.capitalize()}!\" as it flew through the air.")
