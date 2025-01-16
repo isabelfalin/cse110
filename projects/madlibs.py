c6eb60b9fd57bbc9991fce71429e2f99a7d2841a
isabelfalin <fal24007@byui.edu>
2025-01-16 11:14:16 -0700

    madlibs assignment

---
+++ projects/madlibs.py
@@ -0,0 +1,27 @@
+print("Please Enter The Following:")
+print()
+adjective = input("Adjective: ")
+animal = input("Animal: ")
+verb_one = input("Verb: ")
+exclamation = input("Exclamation: ")
+verb_two = input("Verb: ")
+verb_three = input("Verb: ")
+emotion = input("Emotion: ")
+object = input("Object: ")
+action = input("Action: ")
+
+
+
+print()
+print("Here Is Your Story:")
+print()
+
+print(f"""The other day, I was really in trouble. It all started when I saw a very
+{adjective} {animal} {verb_one} down the hallway. "{exclamation.capitalize()}!" I yelled. But all I 
+could think to do was to {verb_two} over and over. Miraculously, 
+that caused it to stop, but not before it tried to {verb_three} 
+right in front of my family. Filled with {emotion} I ran over to the wall and picked up the {object} sitting there,
+I {action} towards the {animal} and threw my new weapon, yelling "{exclamation.capitalize()}!"as it flew through the air.""")
+
+
+
