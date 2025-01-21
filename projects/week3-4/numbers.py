4c02ea5155f5cd9e15613557b55e5617ab70133b
isabelfalin <fal24007@byui.edu>
2025-01-21 10:43:17 -0700

    uhg

--- projects/week3-4/numbers.py
+++ projects/week3-4/numbers.py
@@ -1,5 +1,8 @@
 boys_count = 10
-girls_count = 12
-print(boys_count + girls_count)
+girls_count = "12"
+
+print(boys_count + int(girls_count))
+
 total_count = boys_count + girls_count
+
 print(total_count)
