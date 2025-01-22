bfbaf0a283a3192c42b1f167f309e417388761f9
isabelfalin <fal24007@byui.edu>
2025-01-22 10:36:08 -0700

    rounding practice

---
+++ projects/week3-4/rounding.py
@@ -0,0 +1,16 @@
+cars = 7
+people = 80
+people_per_car = people / cars
+
+#This is without rounding
+print(f"There are {people_per_car} poeple in each car")
+
+#This is with rounding to one
+print(f"There are {people_per_car:.1f} people in each car.")
+
+#This is with rounding to two
+print(f"There are {people_per_car:.2f} people in each car.")
+
+#This is with rounding to three
+print(f"There are {people_per_car:.3f} people in each car.")
+
