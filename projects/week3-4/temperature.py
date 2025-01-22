8a4d5540ae0256d50528a2f9e65cbe7056cd1155
isabelfalin <fal24007@byui.edu>
2025-01-22 10:57:21 -0700

    adding a space between code

--- projects/week3-4/temperature.py
+++ projects/week3-4/temperature.py
@@ -1,5 +1,5 @@
 fahrenheit = float(input("What is the temperature in Fahrenheit? "))
 
 celsius = round((fahrenheit - 32) * 5 / 9, 1)
-
+print()
 print(f"{fahrenheit} degrees F = {celsius} degrees C")
