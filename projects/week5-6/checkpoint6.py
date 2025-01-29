bf3b8f39903ea0119d1efd95f1cf99a2200bd68a
isabelfalin <fal24007@byui.edu>
2025-01-29 12:38:14 -0700

    moreeeeee

--- projects/week5-6/checkpoint6.py
+++ projects/week5-6/checkpoint6.py
@@ -12,10 +12,7 @@ if large >= 5:
         choice = True
     elif (credit >= 7 or income >= 7) and down_payment >= 5:
         choice = True
-else:
-    if credit < 4:
-        choice = False
-    else:
+elif credit >= 4:
         if income >= 7 or down_payment >= 7:
             choice = True
         elif income >= 4 and down_payment >= 4:
