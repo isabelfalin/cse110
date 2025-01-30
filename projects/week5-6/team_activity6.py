4efdab575dbb5a30695f999140fac2b958659672
isabelfalin <fal24007@byui.edu>
2025-01-30 07:37:35 -0700

    team activity 6

--- projects/week5-6/team_activity6.py
+++ projects/week5-6/team_activity6.py
@@ -1,14 +1,25 @@
-
-# What is the age of the first rider? 12
-# What is the height of the first rider? 46
-# Is there a second rider (yes/no)? no
-# Sorry, you may not ride.
+can_ride = True
 rider_age1 = int(input("What is the age of the first rider? "))
 rider_height1 = float(input("WHat is the height of the first rider? "))
-rider_age2 = int(input("What is the age of the second rider? "))
-rider_height2 = float(input("WHat is the height of the second rider? "))
 
 second_rider_question = input("Is there a second rider (yes/no)? ")
+if second_rider_question == "no":
+    #single rider rules
+    if rider_height1 < 36:
+        can_ride = False
+    if rider_age1 < 18 or rider_height1 < 62:
+        can_ride = False
 
-if second_rider_question == True:
-    print("You can ride!")
+else:
+    rider_age2 = int(input("What is the age of the second rider? "))
+    rider_height2 = float(input("WHat is the height of the second rider? "))
+    #double rider rules
+    if rider_height2 < 36 or rider_height1 < 36:
+        can_ride = False
+    if rider_age2 < 18 and rider_age1 < 18:
+        can_ride = False
+
+if can_ride:
+    print("You can ride!!!")
+else:
+    print("You can't ride :( ")
