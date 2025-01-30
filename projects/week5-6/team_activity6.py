bd4cbf7ae6311cd4c63df96b7f51eda84f241ce5
isabelfalin <fal24007@byui.edu>
2025-01-30 09:48:13 -0700

    stretch assignment

--- projects/week5-6/team_activity6.py
+++ projects/week5-6/team_activity6.py
@@ -2,6 +2,12 @@ can_ride = True
 rider_age1 = int(input("What is the age of the first rider? "))
 rider_height1 = float(input("WHat is the height of the first rider? "))
 
+#stretch 2
+if rider_age1 >= 12 and rider_age1 <= 17:
+    passport1 = input("Do you have a golden passport? ")
+    if passport1 == "yes":
+        rider_age1 = 18
+
 second_rider_question = input("Is there a second rider (yes/no)? ")
 if second_rider_question == "no":
     #single rider rules
@@ -14,10 +20,24 @@ else:
     rider_age2 = int(input("What is the age of the second rider? "))
     rider_height2 = float(input("WHat is the height of the second rider? "))
     #double rider rules
+    
+    #stretch 2
+    if rider_age2 >= 12 and rider_age1 <= 17:
+        passport2 = input("Do you have a golden passport? ")
+        if passport2 == "yes":
+            rider_age2 = 18
+
     if rider_height2 < 36 or rider_height1 < 36:
         can_ride = False
     if rider_age2 < 18 and rider_age1 < 18:
-        can_ride = False
+        #strecth 3
+        if (rider_age1 >= 16 and rider_age2 >= 14) or (rider_age1 >= 14 and rider_age2 >= 16):
+            can_ride = True
+        #stretch 1
+        elif rider_age1 > 12 and rider_age2 > 12 and rider_height1 > 52 and rider_height2 > 52:
+            can_ride = True
+        else:
+            can_ride = False
 
 if can_ride:
     print("You can ride!!!")
