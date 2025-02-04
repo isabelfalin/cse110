adf5c7c6533573da09160469107b50b4cac336ff
isabelfalin <fal24007@byui.edu>
2025-02-04 10:17:32 -0700

    checkpoint week 8

---
+++ week7-8/checkpoint8.py
@@ -0,0 +1,18 @@
+colors = ["red", "blue", "green", "yellow"]
+
+for color in colors:
+    print(color)
+
+print()
+
+numbers = range(1, 9)
+
+for number in numbers:
+    print(number)
+
+print()
+
+numbers = range(2, 21, 2)
+
+for number in numbers:
+    print(number)
